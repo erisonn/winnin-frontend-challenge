acb3fadc9aab7b35d8085cc3767662b0096e52df
Erison <erisonn.santos@gmail.com>
2021-07-17 07:00:14 -0300

    Map key

--- src/components/SubsMenu.jsx
+++ src/components/SubsMenu.jsx
@@ -9,7 +9,7 @@ const SubsMenu = ({ sub, defaultSubs }) => {
         <div className="subs-menu">
             <span className="dropdown-menu" onClick={() => setDropDownDisplay(dropDownDisplay => !dropDownDisplay)}>{sub === 'popular'? 'Home' : `r/${sub}`}</span>
             <div className={`dropdown-content ${dropDownDisplay || 'hidden'}`}>
-                {defaultSubs.map(subName => <NavLink to={`/r/${subName.data.display_name}/hot`} activeClassName=''>{subName.data.display_name_prefixed}</NavLink>)}
+                {defaultSubs.map(subName => <NavLink to={`/r/${subName.data.display_name}/hot`} activeClassName='' key={subName.data.id}>{subName.data.display_name_prefixed}</NavLink>)}
             </div>   
         </div>
     );
